15bc4c1d1e2fda853076bb57aedd1e11f186c987
jumbonionga <jumbonionga+git@gmail.com>
2023-04-27 13:08:54 -0600

    Included post date and cleaned title & flair link

--- freebooks.py
+++ freebooks.py
@@ -1,6 +1,14 @@
 from fetchPosts import fetchPosts
+from datetime import datetime
 import json
 
 posts = json.loads(fetchPosts())
 for post in posts:
+    if '&amp;' in post['title']:
+        post['title'] = post['title'].replace('&amp;', '&')
+    if '&amp;' in post['link_flair_text']:
+        post['link_flair_text'] = post['link_flair_text'].replace('&amp;', '&')
     print(f"{post['title']} - {post['url_overridden_by_dest']}")
+    print(f"created: {post['created']}")
+    post['created'] = datetime.fromtimestamp(post['created']).date()
+    print(f"created: {post['created']}")
--- showbooks/views.py
+++ showbooks/views.py
@@ -1,13 +1,29 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from fetchPosts import fetchPosts
+from datetime import datetime
 import json
 
 # Create your views here.
 
 
 def home(request):
+    # Fetch books
     books = json.loads(fetchPosts())
+
+    for book in books:
+        # Change 'created' to actual date format from epoch
+        book['created'] = datetime.fromtimestamp(book['created']).date()
+
+        # Replace '&amp;' to '&' in title
+        if '&amp;' in book['title']:
+            book['title'] = book['title'].replace('&amp;', '&')
+
+        # Replace '&amp;' to '&' in link_flair_text
+        if '&amp;' in book['link_flair_text']:
+            book['link_flair_text'] = book['link_flair_text'].replace(
+                '&amp;', '&')
+
     return render(request, 'books.html', {
         'books': books
     })
